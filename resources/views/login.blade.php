6082e49d8085b14038624958a28121d8439f8b68
Aurjei Steven Carreon <isteb3@gmail.com>
2024-10-13 03:10:33 +0800

    remove checkbox

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -82,14 +82,6 @@
         <input id="password" class="block mt-1 w-full rounded-md border shadow-sm px-3 py-2 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" type="password" name="password" required autocomplete="current-password" placeholder="Your password" />
       </div>
 
-      <!-- Remember Me Checkbox -->
-      <div class="flex items-center mt-4">
-        <label for="remember_me" class="flex items-center text-sm text-gray-600">
-          <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-          <span class="ml-2">Remember me</span>
-        </label>
-      </div>
-
       <!-- Submit and Forgot Password Link -->
       <div class="flex flex-col items-center justify-center mt-4 space-y-4">
         <button type="submit" class="inline-flex items-center px-4 py-2 bg-indigo-600 text-white rounded-md font-medium shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-all duration-150">
